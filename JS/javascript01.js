5ac9a896eebd6aa26907b76045a244b99e8abbcd
CarlosalbCB <carlosalbcastro06@gmail.com>
2017-11-06 15:58:02 -0700

    primera pagina con javascript

---
+++ JS/javascript01.js
@@ -0,0 +1,9 @@
+var datos=function(){
+	 var nombre	= prompt("Teclea tu nombre");
+	 var edad 	= prompt("teclea tu edad");
+	 document.write("Hola ");
+	 document.write(nombre);
+	 document.write(" tienes ");
+	 document.write(edad);
+	 document.write(" años");
+}
