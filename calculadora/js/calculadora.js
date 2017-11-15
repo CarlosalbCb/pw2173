d109a1cb37b381fd7ebd5bfbf34e090312855482
CarlosalbCB <carlosalbcastro06@gmail.com>
2017-11-15 16:01:32 -0700

    empaquetando calculadora con asar

---
+++ calculadora/js/calculadora.js
@@ -0,0 +1,35 @@
+// Variable global
+var operador="";
+//////Se puede usar en cualquier funcion////////
+function numeros(numero){
+	if(operador == ""){ //Escribe en operando1
+		var valorInicial=document.calculadora.operando1.value;
+		if(valorInicial=="0"){
+			document.calculadora.operando1.value="";
+		}
+		//concatena los valores de numero con los del operando1
+		document.calculadora.operando1.value=document.calculadora.operando1.value+numero;
+	}else{ //operando2
+		var valorInicial=document.calculadora.operando2.value;
+		if(valorInicial=="0"){
+			document.calculadora.operando2.value="";
+		}
+		//concatena los valores de numero con los del operando1
+		document.calculadora.operando2.value=document.calculadora.operando2.value+numero;
+	}
+}
+
+var operadores=function(ope){
+	operador=ope;
+}
+
+var borrar=function(){
+	
+}
+
+var igual=function(){
+	if(operador=="+"){
+		var suma=document.calculadora.operando1+document.calculadora.operando2
+		document.calculadora.resultado.value=suma;
+	}
+}
