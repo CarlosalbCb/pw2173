21eb200f7775846830113ad7109410486347bd5d
CarlosalbCB <carlosalbcastro06@gmail.com>
2017-12-07 15:55:13 -0700

    finalizando apartado de alta en proyecto php

--- proyecto/js/menu.js
+++ proyecto/js/menu.js
@@ -31,6 +31,34 @@ var iniciaMenu = function(){
 		});
 
 	}
+	var teclaNControl = function(tecla){
+		if(tecla.which == 13){
+			var ncontrol=$("#txtNcontrol").val();
+			var parametros="opc=buscaNcontrol"+
+							"&ncontrol="+ncontrol+
+							"&id="+Math.random();
+			$.ajax({
+			 url:"php/buscancontrol.php",
+			 dataType: 'json', //retorno
+			 type: "POST", //lo que enviamos
+			 data:parametros,
+			 success:function(data){
+			 	if(data.respuesta == true){
+			 		$('txtNombre').val(data.nombre); 	
+			 		$('txtCarrera').val(data.carrera);
+			 		$('txtClave').val(data.clave);	
+			 	}else{
+			 		$('txtNombre').focus();
+			 	}
+			 },
+			 error:function(a,b,c){
+			 	alert("No se pudo conectar al server");
+			 }
+			});
+		} //13 = enter
+
+	}
+	$("txtNControl").on("keypress",teclaNControl);
 	$("#btnAlta").on("click",alta);
 	$("#btnAltaAlumno").on("click",altaAlumno);
 }
